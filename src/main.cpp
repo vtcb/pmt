f5d4d2dfb39fc7374a543d3888a5292c37d4fce9
Bolado <vtcb@cin.ufpe.br>
2017-09-26 21:16:42 -0300

    Add input parser

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,50 @@ void initFlags() {
   FLAGS.addFlag(&FLAG_HELP);
 }
 
+class InputParser {
+public:
+  static std::vector<std::string> patternList() {
+    std::vector<std::string> pattern_list;
+    if (FLAG_PATTERN.isSet()) {
+      std::ifstream pattern_file(FLAG_PATTERN.getValue());
+      std::string line;
+      while (std::getline(pattern_file, line)) {
+        pattern_list.push_back(line);
+      }
+    } else {
+      pattern_list.push_back(FLAGS.getArgs()[0]);
+    }
+    return pattern_list;
+  }
+
+  static std::vector<std::string> textfileList() {
+    std::vector<std::string> textfile_list;
+    for (int i = FLAG_PATTERN.isSet() ? 0 : 1; i < FLAGS.getArgs().size(); i++) {
+      textfile_list.push_back(FLAGS.getArgs()[i]);
+    }
+    return textfile_list;
+  }
+
+  static SearchMode searchMode() {
+    return FLAG_COUNT.getValue() ? SearchMode::COUNT : SearchMode::PRINT;
+  }
+
+  static SearchAlgorithm* algorithm(SearchMode search_mode) {
+    if (!FLAG_ALGORITHM.isSet()) {
+      // Choose
+      return new Naive(search_mode);
+    }
+
+    if (FLAG_ALGORITHM.getValue() == "brute-force") {
+      return new Naive(search_mode);
+    }
+
+    return nullptr;
+  }
+};
+
 void printHelp() {
+  // TODO(bolado): Write help
   std::cout << "Help me, Obi-Wan Kenobi. You're my only hope." << std::endl;
 }
 
@@ -39,27 +82,20 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
-  std::vector<std::string> pattern_list;
-  std::vector<std::string> textfile_list;
+  std::vector<std::string> pattern_list  = InputParser::patternList();
+  std::vector<std::string> textfile_list = InputParser::textfileList();
+  SearchMode               search_mode   = InputParser::searchMode();
+  SearchAlgorithm          *algorithm    = InputParser::algorithm(search_mode);
 
-  if (FLAG_PATTERN.isSet()) {
-    std::ifstream pattern_file(FLAG_PATTERN.getValue());
-    std::string line;
-    while (std::getline(pattern_file, line)) {
-      pattern_list.push_back(line);
-    }
-  } else {
-    pattern_list.push_back(FLAGS.getArgs()[0]);
+  switch (search_mode) {
+    case SearchMode::COUNT:
+      algorithm->search(pattern_list, textfile_list);
+      std::cout << algorithm->getCount() << std::endl;
+      break;
+    case SearchMode::PRINT:
+      algorithm->search(pattern_list, textfile_list);
+      break;
   }
-
-  for (int i = FLAG_PATTERN.isSet() ? 0 : 1; i < FLAGS.getArgs().size(); i++) {
-    textfile_list.push_back(FLAGS.getArgs()[i]);
-  }
-
-  SearchAlgorithm *algorithm = new Naive(SearchMode::COUNT);
-  algorithm->search(pattern_list, textfile_list);
-  (new Naive(SearchMode::PRINT))->search(pattern_list, textfile_list);
-  std::cout << algorithm->getCount() << std::endl;
 }
 
 /**
