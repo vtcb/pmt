7c210841963ac6874e9ec0fd9e854583e824fca1
Bolado <vtcb@cin.ufpe.br>
2017-10-11 18:51:33 -0300

    Optimize Sellers

--- src/sellers.cc
+++ src/sellers.cc
@@ -27,7 +27,7 @@ void Sellers::search(
 void Sellers::search(
     const std::vector<std::string>& pattern_list, const std::string& text) {
   int matches = 0;
-  for (std::string pattern : pattern_list) {
+  for (const std::string& pattern : pattern_list) {
     matches += search(pattern, text);
   }
   output(text, matches);
@@ -39,19 +39,28 @@ bool Sellers::search(const std::string& pattern, const std::string& text) {
   std::vector<int> T(pattern.size(), infinity);
   int error = infinity;
 
-  auto getT = [&](int di, int dj) -> int {
-    return dj >= 0 ? (di == 0 ? T : T_)[dj] : 0;
-  };
-
-  for (unsigned int i = 0; i < text.size(); i++) {
-    swap(T, T_);
-    for (unsigned int j = 0; j < pattern.size(); j++) {
-      T[j] = infinity;
-      T[j] = std::min(T[j], getT(-1, j)     + 1);
-      T[j] = std::min(T[j], getT(-1, j - 1) + (text[i] != pattern[j]));
-      T[j] = std::min(T[j], getT( 0, j - 1) + 1);
+  auto ITERATION = [&](
+      const std::vector<int>& T,
+      const std::vector<int>& T_,
+      int i) {
+    T[0] = text[i] == pattern[0] ? 0 : 1;
+    for (unsigned int j = 1; j < pattern.size(); j++) {
+      int& cur = T[j];
+      cur = infinity;
+      cur = std::min(cur, T_[j]     + 1);
+      cur = std::min(cur, T_[j - 1] + (text[i] != pattern[j]));
+      cur = std::min(cur, T [j - 1] + 1);
     }
     error = std::min(error, T.back());
+  };
+ 
+  int MAX_SIZE = text.size() & ~1;
+  for (unsigned int i = 0; i < MAX_SIZE; i += 2) {
+    ITERATION(T_, T, i);
+    ITERATION(T, T_, i + 1);
+  }
+  if (text.size() & 1) {
+    ITERATION(T_, T, text.size() - 1);
   }
 
   return error <= getMaxError();
